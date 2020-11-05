8c2506113653d5d5ae6a90e1f51426ce63e83b60
prateek arora <prateekarorav2@gmail.com>
2020-11-05 14:34:47 -0800

    publishing refined base and arm poses

--- publish_viewpoints.py
+++ publish_viewpoints.py
@@ -12,9 +12,10 @@ from Viewpoints import TANK, load_mesh, viewpoint_clusters
 from probabilistic_road_map import prm_planning
 from matplotlib import cm
 from Viewpoints import TANK, create_obstacles, load_mesh, create_viewpoints, viewpoint_clusters
-
+from geometry_msgs.msg import PoseArray, Pose
 MARKER_TOPIC = 'visualization_traj'
 BASE_POSE_TOPIC = 'base_goal_poses'
+ARM_POSE_TOPIC = 'arm_goal_poses'
 
 
 def readPoints():
@@ -29,8 +30,8 @@ def readPoints():
 
     # Probabilistic Road Map algorithm
     print("Running Probabilistic Road Map algorithm on {} clusters.".format(n_clusters))
-    robot_size = .5  # [m]
-    goal_points, feasible_dist, tsp_path = prm_planning(cluster_centers, obstacles, robot_size)
+    robot_size = .6  # [m]
+    goal_points, feasible_dist, tsp_path, samples = prm_planning(cluster_centers, obstacles, robot_size)
 
     # For each goal point, get heading to first viewpoint in that group
     goal_heading_xy = np.zeros((n_clusters, 2))
@@ -42,6 +43,44 @@ def readPoints():
 
     return viewpoints, normals, cluster_groups, goal_points, goal_heading_xy
 
+
+def  pose_from_vector3D(position, dir_vec):
+    #http://lolengine.net/blog/2013/09/18/beautiful-maths-quaternion-from-vectors
+    # print("pos shape = {}, dir shape = {}".format(np.shape(position), np.shape(dir_vec)))
+    pose= Pose()
+    pose.position.x = position[0]
+    pose.position.y = position[1]
+    pose.position.z = position[2] 
+    #calculating the half-way vector.
+    u = np.array([1,0,0])
+    norm = np.linalg.norm(dir_vec)
+    v = np.asarray(dir_vec)/norm 
+    if (np.array_equal(u, v)):
+        pose.orientation.w = 1
+        pose.orientation.x = 0
+        pose.orientation.y = 0
+        pose.orientation.z = 0
+    elif (np.array_equal(u, np.negative(v))):
+        pose.orientation.w = 0
+        pose.orientation.x = 0
+        pose.orientation.y = 0
+        pose.orientation.z = 1
+    else:
+        half = [u[0]+v[0], u[1]+v[1], u[2]+v[2]]
+        pose.orientation.w = np.dot(u, half)
+        temp = np.cross(u, half)
+        pose.orientation.x = temp[0]
+        pose.orientation.y = temp[1]
+        pose.orientation.z = temp[2]
+    norm = math.sqrt(pose.orientation.x*pose.orientation.x + pose.orientation.y*pose.orientation.y + 
+        pose.orientation.z*pose.orientation.z + pose.orientation.w*pose.orientation.w)
+    if norm == 0:
+        norm = 1
+    pose.orientation.x /= norm
+    pose.orientation.y /= norm
+    pose.orientation.z /= norm
+    pose.orientation.w /= norm
+    return pose
 
 def rand_cmap(nlabels, type='bright', first_color_black=True, last_color_black=False, verbose=True):
     """
@@ -114,63 +153,49 @@ def rand_cmap(nlabels, type='bright', first_color_black=True, last_color_black=F
 
     return random_colormap
 
-def main():
-    list_, viewpoint_normals, groups, centers, goal_heading_xy = readPoints()
-    offset = np.array([2, 5, 0])
+def get3DDistance(a, b):
+    return np.linalg.norm(a-b)
 
-    list_ += offset  # offset the model from the origin in Gazebo
-    centers += offset[:2]
+def main():
+    viewpoints, normals, cluster_groups, goal_points, goal_heading_xy = readPoints()
+    # list_, viewpoint_normals, groups, centers, goal_heading_xy = readPoints()
+    offset = np.array([1, 2.5, 0])
+
+    viewpoints += offset  # offset the model from the origin in Gazebo
+    goal_points += offset[:2]
     # cmap = cm.get_cmap('RdBu')
-    cmap = rand_cmap(np.max(groups)+2, type='bright', first_color_black=True, last_color_black=False, verbose=True)
+    print("cluster max",np.max(cluster_groups))
+    cmap = rand_cmap(np.max(cluster_groups)+2, type='bright', first_color_black=True, last_color_black=False, verbose=True)
     
 
-    print("Publishing topic {}".format(topic))
-    publisher = rospy.Publisher(topic, MarkerArray, queue_size=5)
+    # print("Publishing topic {}".format(topic))
+    publisher = rospy.Publisher(MARKER_TOPIC, MarkerArray, queue_size=5)
+    base_pub = rospy.Publisher(BASE_POSE_TOPIC, PoseArray, queue_size=5)
+    arm_pub = rospy.Publisher(ARM_POSE_TOPIC, PoseArray, queue_size=5)
+    
 
     rospy.init_node('register')
 
     markerArray = MarkerArray()
     count = 0
-    MARKERS_MAX = len(list_)
-    r = rospy.Rate(1)
+    # MARKERS_MAX = len(list_)
+    r = rospy.Rate(0.5)
 
-    # Publish the Viewpoint Markers
-    for elem, group in zip(list_, groups):
-        marker = Marker()
-        if(elem[2]>1.5 or elem[2]<0.0):
-            color = cmap(0)
-        else:
-            color = cmap(group)
-        print("color = ", color)
-        # marker.header.frame_id = "/bvr_SIM/bvr_base_inertia"
-        # bvr_SIM/bvr_base_link
-        marker.header.frame_id = "map"
-        marker.type = marker.CUBE
-        marker.action = marker.ADD
-        marker.scale.x = 0.1
-        marker.scale.y = 0.1
-        marker.scale.z = 0.1
-        marker.color.a = color[3]
-        marker.color.r = color[0]
-        marker.color.g = color[1]
-        marker.color.b = color[2]
-        # marker.pose.orientation.x = elem[5]
-        # marker.pose.orientation.y = elem[4]
-        # marker.pose.orientation.z = elem[3]
-        # marker.pose.orientation.w = elem[6]
-        marker.pose.position.x = elem[0]
-        marker.pose.position.y = elem[1]
-        marker.pose.position.z = elem[2] 
-
-        markerArray.markers.append(marker)
-
+    base_poses = PoseArray()
+    arm_poses = PoseArray()
+    print("len of viewpoints = {}, normal = {}, cluster_groups = {}, goal_pts = {}, goal_dir = {}".format(np.shape(viewpoints), np.shape(normals), np.shape(cluster_groups),np.shape(goal_points), np.shape(goal_heading_xy)))
+    
     # Add cluster centers on the ground
-    for i, c in enumerate(centers):
+    i = 0
+    valid_idx = []
+    unique_goal_points = []
+    for base_pos, base_dir in zip(goal_points, goal_heading_xy):
+    # for i, c in enumerate(goal_points):
         marker = Marker()
         color = cmap(i)
-
+        base_pose = Pose()
         marker.header.frame_id = "map"
-        marker.type = marker.SPHERE
+        marker.type = marker.ARROW
         marker.action = marker.ADD
         marker.scale.x = 0.5
         marker.scale.y = 0.5
@@ -179,15 +204,64 @@ def main():
         marker.color.r = color[0]
         marker.color.g = color[1]
         marker.color.b = color[2]
-        # marker.pose.orientation.x = elem[5]
-        # marker.pose.orientation.y = elem[4]
-        # marker.pose.orientation.z = elem[3]
-        # marker.pose.orientation.w = elem[6]
-        marker.pose.position.x = c[0]
-        marker.pose.position.y = c[1]
-        marker.pose.position.z = 0 
+        if( (base_pos[1]<-0.15) or (base_pos[0]<0.8 and base_pos[1]<3.0 )):
+            valid_idx.append(i)
+            if len(unique_goal_points) == 0:
+                base_pose = pose_from_vector3D(np.array([base_pos[0], base_pos[1] ,0.0]), np.array([base_dir[0], base_dir[1], 0.0]))
+                unique_goal_points.append(base_pos)
+                base_poses.poses.append(base_pose)
+                marker.pose = base_pose
+                markerArray.markers.append(marker)
+            else:
+                exist_flag = False
+                for goal in unique_goal_points:
+                    if get3DDistance( np.array([base_pos[0], base_pos[1], 0.0]), np.array([goal[0], goal[1], 0.0]) ) < 0.2:
+                        exist_flag = True
+                        break
+                if exist_flag == False:
+                    base_pose = pose_from_vector3D(np.array([base_pos[0], base_pos[1] ,0.0]), np.array([base_dir[0], base_dir[1], 0.0]))
+                    unique_goal_points.append(base_pos)
+                    base_poses.poses.append(base_pose)
+                    marker.pose = base_pose
+                    markerArray.markers.append(marker)
 
-        markerArray.markers.append(marker)
+        i+=1
+    
+    # Publish the Viewpoint Markers
+    for view_pos, dir_vec, group in zip(viewpoints, normals, cluster_groups):
+        marker = Marker()
+        if(view_pos[2]>1.3 or view_pos[2]<0.0):
+            color = cmap(0)
+            continue
+        else:
+            color = cmap(group)
+        # print("color = ", color)
+        arm_pose = Pose()
+
+        if(group in valid_idx and (view_pos[1]<-0.05 or view_pos[0]<0.9) ):
+        # if(view_pos[1]<-0.15 or view_pos[0]<0.8):
+            arm_pose = pose_from_vector3D(np.array([view_pos[0], view_pos[1] ,view_pos[2]]), np.array([-dir_vec[0], -dir_vec[1], -dir_vec[2]]))
+
+            # marker.header.frame_id = "/bvr_SIM/bvr_base_inertia"
+            # bvr_SIM/bvr_base_link
+            marker.header.frame_id = "map"
+            marker.type = marker.ARROW
+            marker.action = marker.ADD
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
+            marker.color.a = 1.0
+            marker.color.r = color[0]
+            marker.color.g = color[1]
+            marker.color.b = color[2]
+            # marker.pose.orientation.x = view_pos[5]
+            # marker.pose.orientation.y = view_pos[4]
+            # marker.pose.orientation.z = view_pos[3]
+            # marker.pose.orientation.w = view_pos[6]
+            marker.pose = arm_pose
+            markerArray.markers.append(marker)
+            arm_poses.poses.append(arm_pose)
+
 
     # Renumber the marker IDs
     id = 0
@@ -200,6 +274,8 @@ def main():
         # Publish the MarkerArray
     while not rospy.is_shutdown():
         publisher.publish(markerArray)
+        base_pub.publish(base_poses)
+        arm_pub.publish(arm_poses)
 
         count += 1
 
